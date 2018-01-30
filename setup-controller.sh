bd5efd89ed43122a555e8ad6a4aed060149b72c8
Reginald Ferland <rferlan@g.clemson.edu>
2018-01-30 12:51:46 -0500

    Manually merged with fix

--- setup-controller.sh
+++ setup-controller.sh
@@ -4459,16 +4459,16 @@ project_id=`openstack project list -f value | grep admin | cut -d' ' -f 1`
 flavor_id=`openstack flavor list -f value | grep m1.medium | cut -d' ' -f 1`
 image_id=`openstack image list -f value | grep OL7 | cut -d' ' -f 1`
 security_id=`openstack security group list -f value | grep $project_id | cut -d' ' -f 1`
-
 port_id=`openstack port list -f value | grep testport1 | cut -d' ' -f 1`
 
-#Create # of instances
-NUM_INSTANCES = 4
+# See https://docs.openstack.org/python-openstackclient/pike/cli/command-objects/port.html
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.21 testport1
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.22 testport2
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.23 testport3
+openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.24 testport4
 
-for ((i=0; i < NUM_INSTANCES; i++))
-    do
-    # See https://docs.openstack.org/python-openstackclient/pike/cli/command-objects/port.html
-    openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.21 testport${i}
+
+
 
 # See https://docs.openstack.org/mitaka/install-guide-ubuntu/launch-instance-selfservice.html
 openstack server create --flavor m1.medium --security-group $security_id --image OL7 --nic port-id=$port_id headnode
